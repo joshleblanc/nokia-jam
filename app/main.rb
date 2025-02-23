2d9276a598982189f630ec85789185883b663767
Joshua <jleblanc@hey.com>
2025-02-23 16:36:04 -0400

    implementing mill screen

--- app/main.rb
+++ app/main.rb
@@ -41,7 +41,8 @@ MILL = {
   speed: 0,
   production: 0,
   level: 0,
-  purchased: false
+  purchased: false,
+  turn_state: 0,
 }
 
 def boot(args)
@@ -79,15 +80,22 @@ def tick(args)
     h: 1,
   }
 
+  if args.state.confirm_open
+    handle_confirm_input(args)
+  elsif args.state.view == :room
+    handle_room_input(args)
+  end
+
   if args.state.view == :room
-    handle_room_input(args) unless args.state.confirm_open
     render_room_status_bar(args)
     render_room(args)
     # render_info_bar(args)
+  elsif args.state.view == :mill
+    render_mill_status_bar(args)
+    render_mill(args)
   end 
 
-  if args.state.confirm_open
-    handle_confirm_input(args)
+  if args.state.confirm_open 
     render_confirm(args)
   end
 
@@ -112,7 +120,20 @@ def handle_confirm_input(args)
     args.state.confirm_selection = (args.state.confirm_selection - 1).clamp(0, 1)
   elsif args.inputs.keyboard.key_down.right
     args.state.confirm_selection = (args.state.confirm_selection + 1).clamp(0, 1)
+  elsif args.inputs.keyboard.key_down.space
+    process_confirm(args)
   end
+end
+
+def process_confirm(args)
+  if args.state.confirm_selection == 0
+    args.state.confirm_block.call(true)
+  else
+    args.state.confirm_block.call(false)
+  end
+  args.state.confirm_open = false
+  args.state.confirm_block = nil
+  args.state.confirm_selection = 0
 end
 
 def handle_room_input(args)
@@ -132,6 +153,8 @@ end
 def process_mill_select(args)
   mill = args.state.mills[args.state.selection.y][args.state.selection.x]
   if mill.purchased
+    args.state.selected_mill = mill
+    args.state.view = :mill
   else
     confirm(args, "Do you want to buy a mill for 50 money?") do |result|
       if result
@@ -145,9 +168,23 @@ def confirm(args, message, &block)
   args.state.confirm_message = message
   args.state.confirm_block = block
   args.state.confirm_open = true
+end
 
-  
-  # block.call(true)
+def split_text(t, max_w) 
+  lines = []
+  text = []
+  t.split(" ").each do |word|
+    ww,hh = $gtk.calcstringbox((text + [word]).join(" "), size_px: 6, font: "tiny.ttf")
+    if ww >= max_w
+      lines << text.join(" ")
+      text = [word]
+    else 
+      text << word
+    end
+  end
+
+  lines << text.join(" ")
+  lines
 end
 
 def render_confirm(args)
@@ -167,25 +204,10 @@ def render_confirm(args)
     primitive_marker: :border
   }
 
-  w,h = $gtk.calcstringbox(args.state.confirm_message, size_px: 6, font: "tiny.ttf")
-
-  lines = []
-  text = []
-  args.state.confirm_message.split(" ").each do |word|
-    ww,hh = $gtk.calcstringbox((text + [word]).join(" "), size_px: 6, font: "tiny.ttf")
-    if ww >= NOKIA_WIDTH - 8
-      lines << text.join(" ")
-      text = [word]
-    else 
-      text << word
-    end
-  end
-
-  lines << text.join(" ")
-
-  lines.each_with_index do |line, y|
+  p args.state.confirm_message
+  split_text(args.state.confirm_message, NOKIA_WIDTH - 8).each_with_index do |line, y|
     nokia.primitives << {
-      x: 6, y: (NOKIA_WIDTH - 8) / 2 - 4 - y * 6,
+      x: 6, y: (NOKIA_HEIGHT - 8) - y * 6,
       text: line,
       size_px: 6,
       vertical_alignment_enum: 1,
@@ -195,7 +217,44 @@ def render_confirm(args)
     }
   end
 
-  [  ]
+  ["Okay", "Cancel"].each_with_index do |text, x|
+    selected = x == args.state.confirm_selection
+    text_color = selected ? PALLETTE[:secondary] : PALLETTE[:primary]
+    background_color = selected ? PALLETTE[:primary] : PALLETTE[:secondary]
+    
+    w, h = $gtk.calcstringbox(text, size_px: 6, font: "tiny.ttf")
+
+    nokia.primitives << {
+      x: x * 24 + 17, y: 12,
+      text: text,
+      size_px: 6,
+      w: w + 3,
+      h: h + 3,
+      **background_color,
+      primitive_marker: :solid
+    }
+    
+    nokia.primitives << {
+      x: x * 24 + 17, y: 12,
+      text: text,
+      size_px: 6,
+      w: w + 3,
+      h: h + 3,
+      **PALLETTE[:primary],
+      primitive_marker: :border
+    }
+
+    nokia.primitives << {
+      x: x * 24 + 19, y: 14,
+      w: 16, h: 16,
+      text: text,
+      size_px: 6,
+      vertical_alignment_enum: 0,
+      font: "tiny.ttf",
+      **text_color,
+      primitive_marker: :label
+    }
+  end
 end
 
 def render_room(args)
@@ -230,15 +289,38 @@ def render_room(args)
           primitive_marker: :solid
         }
       end
+
+      x_pos = x * MACHINE_SIZE + (13 * (x + 1)) + 8
+      y_pos = y * MACHINE_SIZE + 12
       
       if mill.purchased 
+        if (args.state.tick_count % 60) < 30
+          y_pos -= 1
+          render_mill_45(x_pos, y_pos - 1)
+          
+          # nokia.primitives << {
+          #   x: x_pos - 2,
+          #   x2: x_pos + 2,
+          #   y: y_pos - 1,
+          #   y2: y_pos + 1,
+          #   **PALLETTE[:secondary],
+          #   primitive_marker: :line
+          # }
 
-      else 
-        x_pos = x * MACHINE_SIZE + (13 * (x + 1))
-        y_pos = y * MACHINE_SIZE + 6
-        
+          # nokia.primitives << {
+          #   x: x_pos + 2,
+          #   x2: x_pos - 2,
+          #   y: y_pos - 1,
+          #   y2: y_pos + 1,
+          #   **PALLETTE[:secondary],
+          #   primitive_marker: :line
+          # }
+        else
+          render_mill_90(x_pos, y_pos)
+        end
+      else    
         nokia.primitives << {
-          x: x_pos + 8, y: y_pos + 6,
+          x: x_pos, y: y_pos,
           w: 16, h: 16,
           text: "$",
           size_px: 6,
@@ -251,6 +333,171 @@ def render_room(args)
       end
     end
   end
+end
+
+def render_mill_90(x_pos, y_pos)
+  nokia.primitives << {
+    x: x_pos,
+    x2: x_pos,
+    y: y_pos - 2,
+    y2: y_pos + 2,
+    **PALLETTE[:secondary],
+    primitive_marker: :line
+  }
+
+  nokia.primitives << {
+    x: x_pos + 2,
+    x2: x_pos - 2,
+    y: y_pos,
+    y2: y_pos,
+    **PALLETTE[:secondary],
+    primitive_marker: :line
+  }
+end
+
+def render_mill_45(x_pos, y_pos)
+  nokia.primitives << {
+    x: x_pos,
+    y: y_pos,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+
+  nokia.primitives << {
+    x: x_pos - 1,
+    y: y_pos - 1,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+  nokia.primitives << {
+    x: x_pos - 2,
+    y: y_pos - 2,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+  nokia.primitives << {
+    x: x_pos + 1,
+    y: y_pos + 1,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+  nokia.primitives << {
+    x: x_pos + 2,
+    y: y_pos + 2,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+
+  nokia.primitives << {
+    x: x_pos - 1,
+    y: y_pos + 1,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+  nokia.primitives << {
+    x: x_pos - 2,
+    y: y_pos + 2,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+  nokia.primitives << {
+    x: x_pos + 1,
+    y: y_pos - 1,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+  nokia.primitives << {
+    x: x_pos + 2,
+    y: y_pos - 2,
+    w: 1,
+    h: 1,
+    **PALLETTE[:secondary],
+    primitive_marker: :solid
+  }
+end
+
+def render_mill(args)
+  x_pos = 2
+  y_pos = 18
+  mill = args.state.selected_mill
+
+  nokia.primitives << { 
+    x: x_pos, y: y_pos,
+    w: 16, h: 16,
+    **PALLETTE[:primary],
+    primitive_marker: :solid
+  }
+
+  nokia.primitives << { 
+    x: x_pos + 18, y: y_pos,
+    w: 62, h: 16,
+    **PALLETTE[:primary],
+    primitive_marker: :solid
+  }
+
+  nokia.primitives << {
+    x: x_pos + 20, y: y_pos + 16,
+    **PALLETTE[:secondary],
+    text: "Prod: #{1}",
+    size_px: 6,
+    font: "tiny.ttf",
+    primitive_marker: :label
+  }
+
+  nokia.primitives << {
+    x: x_pos + 20, y: y_pos + 11,
+    **PALLETTE[:secondary],
+    text: "Speed: #{0}",
+    size_px: 6,
+    font: "tiny.ttf",
+    primitive_marker: :label
+  }
+
+  nokia.primitives << {
+    x: x_pos, y: 2,
+    w: NOKIA_WIDTH - 4,
+    h: 14,
+    **PALLETTE[:primary],
+    primitive_marker: :solid
+  }
+
+  split_text("Grind grain with the arrow keys", NOKIA_WIDTH - 4).each_with_index do |line, y|
+    nokia.primitives << {
+      x: x_pos + 2, y: 13 - y * 6,
+      text: line,
+      size_px: 6,
+      vertical_alignment_enum: 1,
+      font: "tiny.ttf",
+      **PALLETTE[:secondary],
+      primitive_marker: :label
+    }
+  end
+  # nokia.primitives << { 
+  #   x: x_pos + 1, y: 16,
+  #   text: "Grind grain with the arrow keys",
+  #   size_px: 6,
+  #   font: "tiny.ttf",
+  #   primitive_marker: :label,
+  #   **PALLETTE[:secondary]
+  # }
+
+  render_mill_90(x_pos + 8, y_pos + 8)
 end
 
 # def render_info_bar(args)
@@ -301,7 +548,7 @@ def render_room_status_bar(args)
   }
 end 
 
-def render_machine_status_bar(args)
+def render_mill_status_bar(args)
   nokia.primitives << { 
     x: 0, y: NOKIA_HEIGHT - STATUS_BAR_H,
     w: NOKIA_WIDTH,
